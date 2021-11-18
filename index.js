cdaa12718e079294d53ee9c3f1dbf73f2c9ff7be
MD Rajib <83833885+blackweb71@users.noreply.github.com>
2021-11-18 12:20:36 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+x = document.querySelector("title");
+a = document.querySelector(".but1");
+b = document.querySelector(".but2");
+c = document.querySelector(".but3");
+
+x.innerHTML="Villain";
+a.innerHTML="Movie";
+b.innerHTML="Web Series";
+c.innerHTML="Natok";
